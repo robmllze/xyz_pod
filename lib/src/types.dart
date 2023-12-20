d737ea10660aae60e835d67d8ab1be91bf1f0b1b
robmllze <86869208+robmllze@users.noreply.github.com>
2023-12-20 16:33:37 +1000

    update

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -13,9 +13,17 @@ import '/xyz_pod.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
+typedef XyzPod = Pod;
+typedef XyzPodBuilder = PodBuilder;
+typedef XyzPodListBuilder = PodListBuilder;
+typedef XyzPodListHelper = PodListHelper;
+typedef XyzResponsivePodListBuilder = ResponsivePodListBuilder;
+
 typedef TPodList<T extends Object?> = Set<Pod<T>?>;
 typedef TPodDataList<T extends Object?> = Set<T>;
 typedef TPodListResponder<T> = TPodList<T> Function();
+
+typedef XyzPodServiceMixin = PodServiceMixin;
 
 mixin PodServiceMixin {
   Future<void> startService();
